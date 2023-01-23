ec7aa31f6621d99c65319c4a99a816e2c7f68225
Abdeali7 <abdeali8411@gmail.com>
2023-01-23 15:06:38 +0400

    changed the whole UI of tihs project

--- app.js
+++ app.js
@@ -1,45 +1,86 @@
-let playerCount = 0;
-let computerCount = 0;
 
-function getComputerChoice() {
-    const rps = ['rock', 'paper', 'scissor'];
-    let computerSelection = rps[Math.floor(Math.random() * rps.length)];
-    return computerSelection;
-}
+let playerCount = 0; // variable to keep track of player's score
+let computerCount = 0; // variable to keep track of computer's score
+let imgs = document.querySelectorAll("img[data-key]");
+let player_points = document.querySelector(".player_points");
+let computer_points = document.querySelector(".computer_points");
+let gameover = document.querySelector("#gameover");
+let gamestart = document.querySelector(".gamestart");
+let gameupdate = document.querySelector(".gameupdate");
+let info = document.querySelector(".info p");
+let playerSelected = document.querySelector(".player img");
+let computerSelected = document.querySelector(".computer img");
+let playerImg;
+let computerImg;
 
-function play(playerSelection, computerSelection){
-    if (playerSelection === 'rock' && computerSelection === 'paper'){
-        computerCount+=1;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissor'){
-        computerCount+=1;
-    } else if (playerSelection === 'scissor' && computerSelection === 'rock'){
-        computerCount+=1;
-    } else if (playerSelection === 'rock' && computerSelection === 'scissor'){
-        playerCount+=1;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        playerCount+=1;
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper'){
-        playerCount+=1;
-    } else {
-        playerCount+=0;
-        computerCount+=0;
+// use a lookup table to determine the outcome of a round
+const outcomes = {
+    rock: { rock: 'tie', paper: 'lose', scissor: 'win' },
+    paper: { rock: 'win', paper: 'tie', scissor: 'lose' },
+    scissor: { rock: 'lose', paper: 'win', scissor: 'tie' },
+  };
+  
+  // function to get random choice for computer
+  function getComputerChoice() {
+      const rps = ['rock', 'paper', 'scissor'];
+      return rps[Math.floor(Math.random() * rps.length)];
+  }
+  
+  for(let img of imgs) {
+      img.addEventListener('click', function (e) {
+          let playerSelection = e.target.attributes[0].value;
+          let computerSelection = getComputerChoice();
+          
+          playerImg = playerSelection;
+          computerImg = computerSelection;
+  
+          play(playerSelection, computerSelection);
+      });
+  }
+  
+  function play(playerSelection, computerSelection){
+      let outcome = outcomes[playerSelection][computerSelection];
+      if(outcome === 'win'){
+          playerCount++;
+          player_points.innerText = playerCount;
+          info.innerText = 'You Win :)';
+          changeImg();
+          checkForWinner();
+      } else if(outcome === 'lose'){
+          computerCount++;
+          computer_points.innerText = computerCount;
+          info.innerText = 'You Loose :(';
+          changeImg();
+          checkForWinner();
+      } else if(outcome === 'tie'){
+          info.innerText = 'Its A Tie :/';
+          changeImg();
+      }
+  }
+  
+  function changeImg(){
+      playerSelected.setAttribute("src", `images/${playerImg}.png`);
+      computerSelected.setAttribute("src", `images/${computerImg}.png`);
+  }
+  
+  function checkForWinner(){
+      if(playerCount === 5) {
+          update('win');
+      } else if(computerCount === 5) {
+          update('lose');
+      }
+  }
+
+  function update(result){
+    gamestart.style.display = "none";
+    gameover.style.display = "block";
+    playerCount = 0;
+    computerCount = 0;
+    player_points.innerText = playerCount;
+    computer_points.innerText = computerCount;
+    if(result === 'win'){
+        gameupdate.innerHTML = 'You Win :) <br> You Stopped Computer To Take Control!';
+    } else if(result === 'lose'){
+        gameupdate.innerHTML = 'You Loose :( <br> Computer Has Taken Control!';
     }
 }
-
-function game(){
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt('Choose One Option : rock, paper, scissors').toLowerCase();
-        const computerSelection = getComputerChoice();
-        play(playerSelection, computerSelection);
-    }
-    if(playerCount > computerCount){
-        return 'You Win :(';
-    } else if (playerCount < computerCount){
-        return 'You Loose :(';
-    } else {
-        return 'Its A Tie :/';
-    }
-}
-
-
-console.log(game());
